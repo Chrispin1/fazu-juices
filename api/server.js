ed22f6d2005cc15f4c1b62e2ce7c456786a3cc87
Chrispin <kiokondolo1@gmail.com>
2025-11-08 23:35:44 +0300

    added json hosting on vercel

--- api/server.js
+++ api/server.js
@@ -1,15 +1,9 @@
 import { create, router as _router, defaults } from "json-server";
-import cors from "cors";
-
 const server = create();
 const router = _router("db.json");
 const middlewares = defaults();
 
-server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+export default server;
